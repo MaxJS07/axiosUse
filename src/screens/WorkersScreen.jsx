3fc0619f9bb013658563986cf968ac1cfbdc9c91
MaxJS07 <maxjimenezsep@gmail.com>
2026-07-23 15:38:49 -0600

    segundo commit

--- src/screens/WorkersScreen.jsx
+++ src/screens/WorkersScreen.jsx
@@ -4,16 +4,17 @@ import { View, FlatList, StyleSheet, Text } from "react-native";
 import CustomCard from "../components/CustomCard";
 //importamos el hook useCustomData que se encargará de obtener la información de los empleados desde la API
 import useCustomData from "../hooks/useCustomData";
+import { stylesWorkersScreen } from "../styles/globalStyles";
 
 const WorkersScreen = () => {
   //utilizamos el hook useCustomData para obtener la información de los empleados y el estado de loading
   const { workerData, loading } = useCustomData();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Lista de empleados</Text>
+    <View style={stylesWorkersScreen.container}>
+      <Text style={stylesWorkersScreen.title}>Lista de empleados</Text>
 
-      <Text style={styles.description}>
+      <Text style={stylesWorkersScreen.description}>
         En esta pantalla estamos mostrando la lista de empleados utilizando el
         componente FlatList de RReact Native. Cada empleado se representa
         mediante un CustomCard que muestra su nombre, trabajo y fecha de inicio.
@@ -29,22 +30,3 @@ const WorkersScreen = () => {
 
 export default WorkersScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#791010",
-    padding: 20,
-  },
-  title: {
-    color: "#fff",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  description: {
-    color: "#fff",
-    fontSize: 16,
-    marginBottom: 20,
-    fontWeight: "semibold",
-  },
-});
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -13,3 +13,39 @@ export const stylesButton = StyleSheet.create({
     textAlign: "center",
   },
 });
+
+export const stylesApp = StyleSheet.create({  
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight || 0 : 50,
+    backgroundColor: "#791010",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+    padding: 20,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight || 0 : 50,
+  },
+});
+
+export const stylesWorkersScreen = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#791010",
+    padding: 20,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  description: {
+    color: "#fff",
+    fontSize: 16,
+    marginBottom: 20,
+    fontWeight: "semibold",
+  },
+});
